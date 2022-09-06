9f955baf6a5c9e2c992a9a7d0520d638e4fad500
João Vitor <202108462691@alunos.unifavip.edu.br>
2022-09-06 16:58:07 -0300

    Create Armstrong Number.c

---
+++ Armstrong Number.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int number, count = 0, result = 0, mul = 0, cnt, rem;
+    printf("Digite um número: ");
+    scanf("%d", &number);
+
+
+
+    int q = number;
+    while(q != 0)
+    {
+        q = q/10;
+        count++;
+    }
+    cnt = count;
+    q = number;
+
+    while (q != 0)
+    {
+        rem = q % 10;
+        while (cnt != 0)
+        {
+            mul = mul * rem;
+            cnt--;
+        }
+        result = result + mul;
+        cnt = count;
+        q = q/10;
+        mul = 1;        
+    }
+    if (result == number)
+    {
+        printf("%d é um número de Armstrong", number);
+    } else {
+        printf("%d não é um número de Armstrong", number);
+    }    
+    return 0;
+}
